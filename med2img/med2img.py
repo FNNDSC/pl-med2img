aea5163b9a79e6deab44950feb7f8027d9a8e0f7
arushivyas <arushi.vyas2096@gmail.com>
2020-05-29 13:16:47 -0400

    Modifications to med2img.py

--- med2img/med2img.py
+++ med2img/med2img.py
@@ -11,9 +11,12 @@
 import os
 
 # import the Chris app superclass
+import sys
+import traceback
+from pydoc import synopsis
+
 from chrisapp.base import ChrisApp
 from med2image import med2image
-
 
 class Med2ImgApp(ChrisApp):
     """
@@ -48,60 +51,89 @@ class Med2ImgApp(ChrisApp):
         """
         Define the CLI arguments accepted by this plugin app.
         """
-        self.add_argument('--outputFileType', dest='outputFileType', type=str,
+        self.add_argument('-i', '--inputFile', dest='inputFile', type=str,
+                          optional=False, help='name of the input file within the inputDir')
+
+        # self.add_argument('-d', dest='outputDir', type=str, optional=False,
+        #                   help='output image Directory', default='') #default?
+        #
+        # self.add_argument('-I', '--inputDir', dest='inputDir', type=str,
+        #                   optional=False, help='input Directory', default='')
+
+        self.add_argument('-o', '--outputFileStem', dest='outputFileStem', type=str, optional=True,
+                          help='output file', default='sample')
+
+        self.add_argument('-t', '--outputFileType', dest='outputFileType', type=str,
                           default='jpg', optional=True, help='output image file format')
-        self.add_argument('--sliceToConvert', dest='sliceToConvert', type=int,
-                          default=-1, optional=True, help='slice to convert (for 3D data)')
+
+        self.add_argument('-s' '--sliceToConvert', dest='sliceToConvert', type=str,
+                          default="-1", optional=True, help='slice to convert (for 3D data)')
+
+        self.add_argument('-f', '--frameToConvert', dest='frameToConvert', type=str,
+                          default="-1", optional=True, help='frame to convert (for 4D data)')
+
+        self.add_argument('--printElapsedTime', dest='printElapsedTime', type=str, action='store_true',
+                          default=False, optional=True, help='print program run time')
+
+        self.add_argument('-r', '--reslice', dest='reslice', type=bool, action='store_true',
+                          default=False, optional=True, help='save images along i, j, k direction -- 3D input only')
+
+        self.add_argument('--showSlices', dest='showSlices', type=bool, action='store_true',
+                          default=False, optional=True, help='show slices that are converted')
+
         self.add_argument('--func', dest='func', type=str, default='', optional=True,
                           help='apply the specified transformation function before saving')
+
+        # self.add_argument('-x', '--man', dest='man', type=bool, action='store_true',
+        #                   default=False, optional=True, help='man')
+
+        self.add_argument('-y', '--synopsis', dest='synopsis', type=bool, action='store_true',
+                          default=False, optional=True, help='short synopsis')
+
+        # self.add_argument('--version', dest='b_version', type=bool, action='store_true',
+        #                   default=False, optional=True, help='if specified, print version number')
+
 
     def run(self, options):
         """
         Define the code to be run by this plugin app.
         """
-        for (dirpath, dirnames, filenames) in os.walk(options.inputdir):
-            output_path = dirpath.replace(options.inputdir, options.outputdir).rstrip('/')
-            for dirname in dirnames:
-                print('Creating directory... %s' % os.path.join(output_path, dirname))
-                os.makedirs(os.path.join(output_path, dirname))
-            for fname in filenames:
-                inputFile = os.path.join(dirpath, fname)
-                try:
-                    str_inputFileStem, str_inputFileExtension = os.path.splitext(inputFile)
-                    b_niftiExt = (str_inputFileExtension == '.nii' or
-                                  str_inputFileExtension == '.gz')
-                    b_dicomExt = str_inputFileExtension == '.dcm'
+        try:
+            options.inputDir = options.inputdir
+            options.outputDir = options.outputdir
+            options.sliceToConvert = options.sliceToConvert
+            options.frameToConvert = options.frameToConvert
 
-                    if b_niftiExt:
-                        C_convert = med2image.med2image_nii(
-                            inputFile=inputFile,
-                            outputDir=output_path,
-                            outputFileStem=fname.split('.')[0],
-                            outputFileType=options.outputFileType,
-                            sliceToConvert=str(options.sliceToConvert),
-                            frameToConvert='-1',
-                            showSlices=False,
-                            reslice=False
-                        )
+            imgConverter = med2image.object_factoryCreate(options).C_convert
 
-                    if b_dicomExt:
-                        C_convert = med2image.med2image_dcm(
-                            inputFile=inputFile,
-                            outputDir=output_path,
-                            outputFileStem='.'.join(fname.split('.')[:-1]),
-                            outputFileType=options.outputFileType,
-                            sliceToConvert=str(options.sliceToConvert),
-                            reslice=False
-                        )
+            if options.version:
+                print("Version: %s" % options.version)
+                sys.exit(1)
 
-                    if options.func:
-                        C_convert.func = options.func
-                    C_convert.run()
+            if options.man or options.synopsis:
+                print(options.synopsis)
+                if options.man:
+                    str_help = synopsis(False)
+                else:
+                    str_help = synopsis(True)
+                print(str_help)
+                sys.exit(1)
 
-                    if b_dicomExt:
-                        break
-                except Exception as e:
-                    print(e)
+            if options.func:
+                imgConverter.func = options.func
+
+            imgConverter.tic()
+            imgConverter.run()
+
+            # if b_dicomExt:
+            #     break
+
+            if options.printElapsedTime:
+                print("Elapsed time = %f seconds" % imgConverter.toc())
+                sys.exit(0)
+
+        except Exception as e:
+            traceback.print_exc()
 
 
 # ENTRYPOINT
