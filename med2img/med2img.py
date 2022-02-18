00d4154c4dcbc3b265f5cb7795e87cce93fe9e27
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2022-02-18 12:56:22 -0500

    Remove man page redundancies, fix man page handling, fix some internal logic flow.

--- med2img/med2img.py
+++ med2img/med2img.py
@@ -15,8 +15,9 @@ import  os
 
 import  sys
 
-from chrisapp.base  import ChrisApp
-from med2image      import med2image
+from    chrisapp.base  import ChrisApp
+from    med2image      import med2image
+import  pudb
 
 Gstr_title = """
                     _  _____ _                 
@@ -29,7 +30,7 @@ Gstr_title = """
                                          |___/ 
 """
 
-Gstr_synopsis = """
+shortSynopsis = '''
 
     NAME
 
@@ -38,6 +39,15 @@ Gstr_synopsis = """
     SYNOPSIS
 
         med2img                                                         \\
+            [--inputFileSubStr <substr>]                                \\
+            [-o|--output <outputFileStem>]                              \\
+            [-t|--outputFileType <outputFileType>]                      \\
+            [-s|--sliceToConvert <sliceToConvert>]                      \\
+            [--convertOnlySingleDICOM]                                  \\
+            [-f|--frameToConvert <frameToConvert>]                      \\
+            [--showSlices]                                              \\
+            [--func <functionName>]                                     \\
+            [--reslice]                                                 \\
             [-h] [--help]                                               \\
             [--json]                                                    \\
             [--man]                                                     \\
@@ -56,7 +66,9 @@ Gstr_synopsis = """
                 -v $(pwd)/in:/incoming -v $(pwd)/out:/outgoing          \\
                 fnndsc/pl-med2img med2img                               \\
                 /incoming /outgoing
+'''
 
+description = '''
     DESCRIPTION
 
         The `med2img` plugin is a thin wrapper about a `med2image` module. 
@@ -168,8 +180,7 @@ Gstr_synopsis = """
         'j' directions are also saved. Furthermore, the <outputDir> is subdivided into
         'slice' (k), 'row' (i), and 'col' (j) subdirectories.
 
-"""
-
+'''
 
 class Med2img(ChrisApp):
     """
@@ -312,12 +323,6 @@ class Med2img(ChrisApp):
         options.inputDir        = options.inputdir
         options.outputDir       = options.outputdir
 
-        imgConverter = med2image.object_factoryCreate(options).C_convert
-
-        if options.version:
-            print("Version: %s" % options.version)
-            sys.exit(1)
-
         if options.man or options.synopsis:
             if options.man:
                 str_help = self.show_man_page(False)
@@ -325,6 +330,8 @@ class Med2img(ChrisApp):
                 str_help = self.show_man_page(True)
             print(str_help)
             sys.exit(1)
+
+        imgConverter = med2image.object_factoryCreate(options).C_convert
 
         if options.func:
             imgConverter.func = options.func
@@ -336,130 +343,8 @@ class Med2img(ChrisApp):
             print("Elapsed time = %f seconds" % imgConverter.toc())
             sys.exit(0)
 
-
-    def show_man_page(self, ab_shortOnly=False):
+    def show_man_page(self):
         """
         Print the app's man page.
         """
-        scriptName = os.path.basename(sys.argv[0])
-        shortSynopsis = '''
-        NAME
-    	    pl-med2img - convert medical images to jpg/png/etc.
-        SYNOPSIS
-                %s                                       \\
-                        [-i|--input <inputFile>]                \\
-                        [--inputFileSubStr <substr>]            \\
-                        [-d|--outputDir <outputDir>]            \\
-                        [-o|--outputFileStem <outputFileStem>]  \\
-                        [-t|--outputFileType <outputFileType>]  \\
-                        [-s|--sliceToConvert <sliceToConvert>]  \\
-                        [--convertOnlySingleDICOM]              \\
-                        [-f|--frameToConvert <frameToConvert>]  \\
-                        [--showSlices]                          \\
-                        [--func <functionName>]                 \\
-                        [--reslice]                             \\
-                        [-x|--man]                              \\
-                        [-y|--synopsis]
-        ''' % scriptName
-
-        description = '''
-        DESCRIPTION
-            `%s' converts input medical image formatted data to a more
-            display friendly format, such as jpg or png.
-            Currently understands NIfTI and DICOM input formats.
-        ARGS
-            [-i|--inputFile <inputFile>]
-            Input file to convert. Typically a DICOM file or a nifti volume.
-
-            [--inputFileSubStr <substr>]
-            As a convenience, the input file can be determined via a substring
-            search of all the files in the <inputDir> using this flag. The first
-            filename hit that contains the <substr> will be assigned the
-            <inputFile>.
-
-            This flag is useful is input names are long and cumbersome, but
-            a short substring search would identify the file. For example, an
-            input file of
-
-               0043-1.3.12.2.1107.5.2.19.45152.2013030808110149471485951.dcm
-
-            can be specified using ``--inputFileSubStr 0043-``
-
-            [-d|--outputDir <outputDir>]
-            The directory to contain the converted output image files.
-
-            [-o|--outputFileStem <outputFileStem>]
-            The output file stem to store conversion. If this is specified
-            with an extension, this extension will be used to specify the
-            output file type.
-
-            SPECIAL CASES:
-            For DICOM data, the <outputFileStem> can be set to the value of
-            an internal DICOM tag. The tag is specified by preceding the tag
-            name with a percent character '%%', so
-
-                -o %%ProtocolName
-
-            will use the DICOM 'ProtocolName' to name the output file. Note
-            that special characters (like spaces) in the DICOM value are
-            replaced by underscores '_'.
-            
-            Multiple tags can be specified, for example
-
-                -o %%PatientName%%PatientID%%ProtocolName
-
-            and the output filename will have each DICOM tag string as
-            specified in order, connected with dashes.
-
-            A special %%inputFile is available to specify the input file that
-            was read (without extension).
-
-            [--convertOnlySingleDICOM]
-            If specified, will only convert the single DICOM specified by the
-            '--inputFile' flag. This is useful for the case when an input
-            directory has many DICOMS but you specifially only want to convert
-            the named file. By default the script assumes that multiple DICOMS
-            should be converted en mass otherwise.
-
-            [-t|--outputFileType <outputFileType>]
-            The output file type. If different to <outputFileStem> extension,
-            will override extension in favour of <outputFileType>.
-
-            [-s|--sliceToConvert <sliceToConvert>]
-            In the case of volume files, the slice (z) index to convert. Ignored
-            for 2D input data. If a '-1' is sent, then convert *all* the slices.
-            If an 'm' is specified, only convert the middle slice in an input
-            volume.
-
-            [-f|--frameToConvert <sliceToConvert>]
-            In the case of 4D volume files, the volume (V) containing the
-            slice (z) index to convert. Ignored for 3D input data. If a '-1' is
-            sent, then convert *all* the frames. If an 'm' is specified, only
-            convert the middle frame in the 4D input stack.
-
-            [--showSlices]
-            If specified, render/show image slices as they are created.
-
-            [--func <functionName>]
-            Apply the specified transformation function before saving. Currently
-            support functions:
-                * invertIntensities
-                  Inverts the contrast intensity of the source image.
-
-            [--reslice]
-            For 3D data only. Assuming [i,j,k] coordinates, the default is to save
-            along the 'k' direction. By passing a --reslice image data in the 'i' and
-            'j' directions are also saved. Furthermore, the <outputDir> is subdivided into
-            'slice' (k), 'row' (i), and 'col' (j) subdirectories.
-
-            [-x|--man]
-            Show full help.
-
-            [-y|--synopsis]
-            Show brief help.
-                ''' % (scriptName)
-
-        if ab_shortOnly:
-            return shortSynopsis
-        else:
-            return shortSynopsis + description
+        print(Gstr_title + shortSynopsis + description)
